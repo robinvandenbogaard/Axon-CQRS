5e00429bf46684621a80e48343653bd2dc3abbb0
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2024-03-22 22:40:17 +0100

    Initial commit

---
+++ src/test/java/nl/robinthedev/app/AxonApplicationTest.java
@@ -0,0 +1,13 @@
+package nl.robinthedev.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
