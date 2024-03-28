987f48add51e44c418fd464bb8da728bf4a34b93
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2024-03-28 22:56:55 +0100

    Restore IT tests

---
+++ posts-parent/posts-projection/src/test/java/nl/robinthedev/app/post/projection/PostEventHandlerIT.java
@@ -0,0 +1,62 @@
+package nl.robinthedev.app.post.projection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.UUID;
+import nl.robinthedev.app.api.messaging.post.event.CommentAdded;
+import nl.robinthedev.app.api.messaging.post.event.CommentUpdated;
+import nl.robinthedev.app.api.messaging.post.event.PostCreated;
+import nl.robinthedev.app.api.messaging.post.model.Comment;
+import nl.robinthedev.app.api.messaging.post.model.CommentId;
+import nl.robinthedev.app.api.messaging.post.model.Post;
+import nl.robinthedev.app.api.messaging.post.model.PostId;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class PostEventHandlerIT {
+
+  public static final UUID POST_UUID = UUID.randomUUID();
+  private static final PostId POST_ID = new PostId(POST_UUID);
+  public static final UUID COMMENT_UUID = UUID.randomUUID();
+  private static final CommentId COMMENT_ID = new CommentId(COMMENT_UUID);
+
+  @Autowired private PostEventHandler handler;
+
+  @Autowired private PostRepository postRepository;
+
+  @Test
+  void createdPost() {
+    handler.handle(new PostCreated(POST_ID, new Post("x", "y")));
+    assertThat(postRepository.findByPublicId(POST_UUID)).isPresent();
+  }
+
+  @Test
+  void addedComment() {
+    handler.handle(new PostCreated(POST_ID, new Post("x", "y")));
+    handler.handle(new CommentAdded(POST_ID, new Comment(COMMENT_ID, "my comment")));
+    assertThat(postRepository.findByPublicId(POST_UUID))
+        .hasValueSatisfying(
+            jpaPost -> {
+              JpaComment comment = jpaPost.getComments().getFirst();
+              assertThat(comment.getText()).isEqualTo("my comment");
+              assertThat(comment.getPublicId()).isEqualTo(COMMENT_ID.commentId());
+            });
+  }
+
+  @Test
+  void updatedComment() {
+    handler.handle(new PostCreated(POST_ID, new Post("x", "y")));
+    handler.handle(new CommentAdded(POST_ID, new Comment(COMMENT_ID, "my comment")));
+    handler.handle(new CommentUpdated(COMMENT_ID, "my comment", "my new comment"));
+    assertThat(postRepository.findByPublicId(POST_UUID))
+        .hasValueSatisfying(
+            jpaPost ->
+                assertThat(jpaPost.getComments().getFirst().getText()).isEqualTo("my new comment"));
+  }
+}
---
+++ posts-parent/posts-projection/src/test/java/nl/robinthedev/app/post/projection/PostQueryControllerIT.java
@@ -0,0 +1,44 @@
+package nl.robinthedev.app.post.projection;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql("posts.sql")
+@Transactional
+@AutoConfigureMockMvc
+class PostQueryControllerIT {
+
+  @Autowired private MockMvc mockMvc;
+
+  @Test
+  void listAllPosts() throws Exception {
+    mockMvc
+        .perform(get("/api/posts"))
+        .andExpect(status().isOk())
+        .andExpect(
+            content()
+                .json(
+                    """
+                    [{
+                      "id": "52b9d5d0-4959-4c3d-a390-b415ec9dfaaa",
+                      "title": "Story of my life",
+                      "text": "Let me tell you what happened.",
+                      comments: [{
+                        "id": "ce6ef61b-292a-4b30-a172-d5355a6c6e19",
+                        "text": "Unbelievable!"
+                      }]
+                    }]"""));
+  }
+}
